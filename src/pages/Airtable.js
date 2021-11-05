8f68628377ef89bdc51e32c2c32b123ddea8f422
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-11-05 18:53:52 +0530

    1 file modified

--- src/pages/Airtable.js
+++ src/pages/Airtable.js
@@ -5,55 +5,41 @@ import Col from 'react-bootstrap/Col';
 import { Avatar } from './components/Avatar';
 export default function Airtable() {
   const [users, setUser] = useState(null);
+  const [posts, setPosts] = useState(null);
   useEffect(() => {
     fetch(
-      'https://api.airtable.com/v0/app1uk6s6TI6oRTxZ/Users?maxRecords=3&view=Grid%20view&&api_key=keyeNXyxxuuYJY19w'
+      'https://api.airtable.com/v0/app1uk6s6TI6oRTxZ/Users?&view=Grid%20view&&api_key=keyeNXyxxuuYJY19w'
     )
       .then((res) => res.json())
       .then((data) => setUser(data.records))
       .catch((error) => console.log(error));
   });
+  useEffect(() => {
+    fetch(
+      'https://api.airtable.com/v0/app1uk6s6TI6oRTxZ/Posts?&view=Grid%20view&&api_key=keyeNXyxxuuYJY19w'
+    )
+      .then((res) => res.json())
+      .then((data) => setPosts(data.records))
+      .catch((error) => console.log(error));
+  });
+  console.log(posts);
+
   const ListItem = ({ item }) => {
     return (
-      <div key={item.Name}>
+      <li>
         <div>{item.Name}</div>
         <div>{item.Address}</div>
         <div>{item.Status}</div>
-      </div>
+      </li>
     );
   };
-  // const records = () => {
-  //   fetch(
-  //     'https://api.airtable.com/v0/app1uk6s6TI6oRTxZ/Users?maxRecords=3&view=Grid%20view&&api_key=keyeNXyxxuuYJY19w'
-  //   )
-  //     .then((res) => res.json())
-  //     // .then(res => {
-  //     // 	console.log(res.records)
-  //     // 	// this.setState({ booksData: res.records })
-  //     // })
-  //     .then((data) => setUser(data))
-  //     .catch((error) => console.log(error));
-  // };
-  // console.log(users);
-
-  // useEffect(() => {
-  //   fetch('https://jsonplaceholder.typicode.com/posts/1')
-  //     .then((response) => response.json())
-  //     // 4. Setting *photos* to the image url that we received from the response above
-  //     .then((data) => setposts(data));
-  // }, []);
-
   return (
     <div>
-      Airtable1
+      Airtable
       {users &&
         users.map((item) => {
           // console.log(item);
-          return (
-            <>
-              <ListItem key={item.id} item={item.fields} />
-            </>
-          );
+          return <ListItem item={item.fields} key={item.id} />;
         })}
       {/* {users ? <ListItem /> : null} */}
     </div>
