cbd52e1347a915924685bbafc0585dda6ef50c24
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-11-06 12:52:34 +0530

    1 file modified, 1 added

---
+++ src/core/UserPhoto.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function UserPhoto({ user }) {
+  return <div></div>;
+}
--- src/pages/Airtable.js
+++ src/pages/Airtable.js
@@ -4,10 +4,20 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Avatar } from './components/Avatar';
 import { Counter } from '../features/counter/Counter';
+import { UserPhoto } from '../core/UserPhoto';
 
 export default function Airtable() {
   const [users, setUser] = useState(null);
   const [posts, setPosts] = useState(null);
+  const url1 =
+    'https://api.airtable.com/v0/app1uk6s6TI6oRTxZ/Users?&view=Grid%20view&&api_key=keyeNXyxxuuYJY19w';
+  const url2 =
+    'https://api.airtable.com/v0/app1uk6s6TI6oRTxZ/Posts?&view=Grid%20view&&api_key=keyeNXyxxuuYJY19w';
+
+  Promise.all([fetch(url1), fetch(url2)]).then(([item1, item2]) => {
+    console.log(item1);
+    console.log(item2);
+  });
   useEffect(() => {
     fetch(
       'https://api.airtable.com/v0/app1uk6s6TI6oRTxZ/Users?&view=Grid%20view&&api_key=keyeNXyxxuuYJY19w'
@@ -16,14 +26,14 @@ export default function Airtable() {
       .then((data) => setUser(data.records))
       .catch((error) => console.log(error));
   });
-  // useEffect(() => {
-  //   fetch(
-  //     'https://api.airtable.com/v0/app1uk6s6TI6oRTxZ/Posts?&view=Grid%20view&&api_key=keyeNXyxxuuYJY19w'
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => setPosts(data.records))
-  //     .catch((error) => console.log(error));
-  // });
+  useEffect(() => {
+    fetch(
+      'https://api.airtable.com/v0/app1uk6s6TI6oRTxZ/Posts?&view=Grid%20view&&api_key=keyeNXyxxuuYJY19w'
+    )
+      .then((res) => res.json())
+      .then((data) => setPosts(data.records))
+      .catch((error) => console.log(error));
+  });
   // console.log(posts);
 
   const ListItem = ({ item }) => {
@@ -53,15 +63,15 @@ export default function Airtable() {
       <h1>Users</h1>
       {users &&
         users.map((item) => {
-          // console.log(item);
           return (
             <div>
               <ListItem item={item.fields} key={item.id} />
+              <UserPhoto user={item.fields.photo} />
               <Counter />
             </div>
           );
         })}
-      {/* <h1>Posts</h1>
+      <h1>Posts</h1>
       {posts &&
         posts.map((item) => {
           // console.log(item);
@@ -71,7 +81,7 @@ export default function Airtable() {
               <Counter />
             </div>
           );
-        })} */}
+        })}
     </div>
   );
 }
