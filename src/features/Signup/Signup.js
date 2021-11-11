7627a69a810d7a66d2999d0583863e70f1b15732
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-11-11 15:44:15 +0530

    1 file modified

--- src/features/Signup/Signup.js
+++ src/features/Signup/Signup.js
@@ -2,7 +2,25 @@ import React from 'react';
 import { Form, Formik, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 export default function Signup() {
-  const SignupSchema = yup.scheme;
+  const SignupSchema = Yup.object().shape({
+    displayname: Yup.string()
+      .min(2, 'Too Short')
+      .max(20, 'Too long')
+      .required('Required'),
+    firstname: Yup.string()
+      .min(2, 'Too short')
+      .max(20, 'Too long')
+      .required('Required'),
+    lastname: Yup.string()
+      .min(2, 'Too short')
+      .max(20, 'Too long')
+      .required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+    password: Yup.string()
+      .min(2, 'Too short')
+      .max(20, 'Too long')
+      .required('Required'),
+  });
   return (
     <div>
       <h1>Signup</h1>
@@ -12,6 +30,7 @@ export default function Signup() {
           firstname: '',
           lastname: '',
           email: '',
+          password: '',
         }}
         validationSchema={SignupSchema}
         // validate={(values) => {
@@ -37,9 +56,24 @@ export default function Signup() {
           /* and other goodies */
         }) => (
           <Form className="form">
-            <Field type="email" name="email" />
+            <Field type="text" name="displayname" placeholder="displayname" />
+            <ErrorMessage
+              name="displayname"
+              component="label"
+              className="error"
+            />
+            <Field type="text" name="firstname" placeholder="firstname" />
+            <ErrorMessage
+              name="firstname"
+              component="label"
+              className="error"
+            />
+            <Field type="text" name="lastname" placeholder="lastname" />
+            <ErrorMessage name="lastname" component="label" className="error" />
+
+            <Field type="email" name="email" placeholder="email" />
             <ErrorMessage name="email" component="label" className="error" />
-            <Field type="password" name="password" />
+            <Field type="password" name="password" placeholder="password" />
             <ErrorMessage name="password" component="label" className="error" />
             <button type="submit" disabled={isSubmitting}>
               Submit
