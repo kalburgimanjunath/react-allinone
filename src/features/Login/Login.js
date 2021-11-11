95164ba4e0bd9e819eb079f9b55694d644d1e387
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-11-11 13:46:42 +0530

    3 files modified

--- src/features/Login/Login.js
+++ src/features/Login/Login.js
@@ -1,31 +1,44 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 export default function Login() {
   return (
     <div>
       <h1>Login</h1>
-      <div>
-        <div>Username:</div>
-        <div>
-          <input type="text" />
-        </div>
-      </div>
-      <div>
-        <div>Password:</div>
-        <div>
-          <input type="password" />
-        </div>
-      </div>
-      <div>
-        <div>
-          <br />
-          <button type="button" className="btn btn-primary">
-            Sign In
-          </button>
-          <button type="button" className="btn btn-primary">
-            Sign Up
-          </button>
-        </div>
-      </div>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            console.log(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+          <Form className="form">
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="label" className="error" />
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="label" className="error" />
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
