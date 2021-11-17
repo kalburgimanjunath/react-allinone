f0a2062c22498c92c4537bc141439c11feb21a86
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-11-17 21:12:43 +0530

    1 file modified

--- src/features/Movie/Movie.js
+++ src/features/Movie/Movie.js
@@ -4,12 +4,26 @@ export default function Movie() {
   let postItem;
   useEffect(() => {
     fetch(
-      'https://api.themoviedb.org/3/movie/550?api_key=9ff11b48756361f4d7fb594dbbb49e87'
+      'https://api.themoviedb.org/4/list/1?page=1&api_key=9ff11b48756361f4d7fb594dbbb49e87'
     )
       .then((response) => response.json())
       // 4. Setting *photos* to the image url that we received from the response above
-      .then((data) => setMovies(data));
+      .then((data) => setMovies(data.results));
   }, []);
+  const IMG_API = 'https://image.tmdb.org/t/p/w500/';
   console.log(movies);
-  return <div>{movies.original_title}</div>;
+  return (
+    <div>
+      {movies.map((item) => {
+        return (
+          <>
+            <div>{item.original_title}</div>
+            <div>
+              <img src={IMG_API + item.poster_path} />
+            </div>
+          </>
+        );
+      })}
+    </div>
+  );
 }
