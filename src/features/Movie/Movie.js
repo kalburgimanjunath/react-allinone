1683bdccdbec44d281a7d3f1747feeb55c17f291
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-11-18 12:04:55 +0530

    2 files modified, 1 added

--- src/features/Movie/Movie.js
+++ src/features/Movie/Movie.js
@@ -1,18 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import { getLatestMovies } from '../../services/apiConnect';
+import axios from 'axios';
 export default function Movie() {
+  const baseUrl = 'https://api.themoviedb.org';
+  const api_KEY = '9ff11b48756361f4d7fb594dbbb49e87';
   let [movies, setMovies] = useState([]);
   let postItem;
+  
   useEffect(() => {
     fetch(
       'https://api.themoviedb.org/4/list/1?page=1&api_key=9ff11b48756361f4d7fb594dbbb49e87'
     )
       .then((response) => response.json())
-      // 4. Setting *photos* to the image url that we received from the response above
+
       .then((data) => setMovies(data.results));
-      
-  }, []);
+  });
   const IMG_API = 'https://image.tmdb.org/t/p/w500/';
-  console.log(movies);
+  // console.log(movies);
+  // getLatestMovies;
+
   return (
     <div>
       {movies.map((item) => {
